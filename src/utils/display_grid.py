c688bfaee49e935741b95ead110b3675d5233848
Maxime Brisinger <maximebrisinger@gmail.com>
2021-10-15 17:39:38 +0200

    -

--- src/utils/display_grid.py
+++ src/utils/display_grid.py
@@ -8,7 +8,6 @@ def display(grid):
     for i in range(x):
         for j in range(y):
             if grid[i, j] == 1:
-                print((i, j))
                 plt.scatter(i, j, marker="+", color='blue')
             elif grid[i, j] == 2:
                 plt.scatter(i, j, marker="o", color='red')
